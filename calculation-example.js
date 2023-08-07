d9348d9aebc11e5781a0e6d5840e8e53f36c457f
Sean Matthew <s.matthew.english@gmail.com>
2023-08-06 20:31:44 -0400

    update

--- calculation-example.js
+++ calculation-example.js
@@ -1,6 +1,3 @@
-// This example shows how to calculate a continuously compounding interested rate.
-// This calculation would require significant on-chain gas, but is easy for a decentralized oracle network.
-
 const config = {
   url: `https://api.coingecko.com/api/v3/simple/price?ids=dimo&vs_currencies=usd`,
   method: "GET",
@@ -9,10 +6,4 @@ const config = {
 const response = await Functions.makeHttpRequest(config)
 const dimoPrice = response.data.dimo.usd * 10 ** 18
 
-// The source code MUST return a Buffer or the request will return an error message
-// Use one of the following functions to convert to a Buffer representing the response bytes that are returned to the client smart contract:
-// - Functions.encodeUint256
-// - Functions.encodeInt256
-// - Functions.encodeString
-// Or return a custom Buffer for a custom byte encoding
 return Functions.encodeUint256(dimoPrice)
